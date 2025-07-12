33f8d80b33df52916bc0e075026831de4f22e2a0
Jai-Rajput007 <136909371+Jai-Rajput007@users.noreply.github.com>
2025-07-12 17:20:00 +0530

    final

--- Backend/app.js
+++ Backend/app.js
@@ -7,10 +7,6 @@ const authRoutes = require("./routes/authRoutes");
 const userRoutes = require("./routes/userRoutes");
 const itemRoutes = require("./routes/itemRoutes");
 const swapRoutes = require("./routes/swapRoutes");
-<<<<<<< HEAD
-=======
-const redeemRoutes = require("./routes/redeemRoutes");
->>>>>>> a18838929c1d1b424a82a7fdffa862ca161457be
 
 const app = express();
 
@@ -26,23 +22,14 @@ app.use("/api/auth", authRoutes);
 app.use("/api/user", userRoutes);
 app.use("/api/items", itemRoutes);
 app.use("/api/swaps", swapRoutes);
-<<<<<<< HEAD
 
-=======
-app.use("/api/items", redeemRoutes);
->>>>>>> a18838929c1d1b424a82a7fdffa862ca161457be
 
 // Root
 app.get("/", (req, res) => {
   res.send("ReWear backend is connected to MongoDB!");
 });
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
-
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`🚀 Server is running on port ${PORT}`);
 });
